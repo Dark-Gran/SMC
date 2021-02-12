9e016937607e6a6de1651581aea7267b16ae1165
Dark-Gran <filip.randak@gmail.com>
2021-02-12 02:47:51 +0100

    circle "teleport" on screen-edge

--- core/src/com/darkgran/smc/play/ColoredCircle.java
+++ core/src/com/darkgran/smc/play/ColoredCircle.java
@@ -1,6 +1,8 @@
 package com.darkgran.smc.play;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.darkgran.smc.WorldScreen;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -24,17 +26,35 @@ public class ColoredCircle {
     }
 
     public void update() {
-        double currentSpeed = Math.sqrt(Math.pow(circleBody.getDynamicBody().getLinearVelocity().x, 2) + Math.pow(circleBody.getDynamicBody().getLinearVelocity().y, 2));
+        Body body = circleBody.getBody();
+        //Speed Cap
+        double currentSpeed = Math.sqrt(Math.pow(body.getLinearVelocity().x, 2) + Math.pow(body.getLinearVelocity().y, 2));
         if ((float) currentSpeed != speed) {
-            double x = speed * cos(angle);
-            double y = speed * sin(angle);
-            circleBody.getDynamicBody().setLinearVelocity((float) x, (float) y);
+            double speedX = speed * cos(angle);
+            double speedY = speed * sin(angle);
+            body.setLinearVelocity((float) speedX, (float) speedY);
+        }
+        //Screen Edge
+        if (body.getPosition().x-radius > WorldScreen.WORLD_WIDTH || body.getPosition().x+radius < 0 || body.getPosition().y-radius > WorldScreen.WORLD_HEIGHT || body.getPosition().y+radius < 0) {
+            float newX = body.getPosition().x;
+            float newY = body.getPosition().y;
+            if (body.getPosition().x > WorldScreen.WORLD_WIDTH) {
+                newX = 0-radius;
+            } else if (body.getPosition().x < 0) {
+                newX = WorldScreen.WORLD_WIDTH+radius;
+            }
+            if (body.getPosition().y > WorldScreen.WORLD_HEIGHT) {
+                newY = 0-radius;
+            } else if (body.getPosition().y < 0) {
+                newY = WorldScreen.WORLD_HEIGHT+radius;
+            }
+            body.setTransform(newX, newY, body.getAngle());
         }
     }
 
     public void drawShapes(ShapeRenderer shapeRenderer) {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.circle(circleBody.getDynamicBody().getPosition().x, circleBody.getDynamicBody().getPosition().y, radius, Math.round(radius*100));
+        shapeRenderer.circle(circleBody.getBody().getPosition().x, circleBody.getBody().getPosition().y, radius, Math.round(radius*100));
         shapeRenderer.end();
     }
 
