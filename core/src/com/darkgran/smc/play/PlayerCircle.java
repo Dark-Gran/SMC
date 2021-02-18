2654392a9458b38388708acc695246b99325be22
Dark-Gran <filip.randak@gmail.com>
2021-02-18 23:00:18 +0100

    todo

--- core/src/com/darkgran/smc/play/PlayerCircle.java
+++ core/src/com/darkgran/smc/play/PlayerCircle.java
@@ -2,7 +2,7 @@ package com.darkgran.smc.play;
 
 import com.badlogic.gdx.physics.box2d.BodyDef;
 
-public class PlayerCircle extends CircleActor {
+public class PlayerCircle extends CircleActor { //TODO sprite
 
     public PlayerCircle(LevelStage levelStage, float x, float y, float radius) {
         super(levelStage, x, y, radius, BodyDef.BodyType.StaticBody);
