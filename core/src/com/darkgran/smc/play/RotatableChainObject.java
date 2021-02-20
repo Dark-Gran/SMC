7f50169b3e6716eeb01b95797b3adbfc18167853
Dark-Gran <filip.randak@gmail.com>
2021-02-20 23:33:56 +0100

    Switchable implementation

--- core/src/com/darkgran/smc/play/RotatableChainObject.java
+++ core/src/com/darkgran/smc/play/RotatableChainObject.java
@@ -2,12 +2,29 @@ package com.darkgran.smc.play;
 
 import com.badlogic.gdx.math.Vector2;
 
-public abstract class RotatableChainObject extends ChainBodyObject {
+public abstract class RotatableChainObject extends ChainBodyObject implements Switchable {
     private float angle;
 
     public RotatableChainObject(LevelStage levelStage, float x, float y, Vector2[] vertices, float angle, float restitution) {
         super(levelStage, x, y, vertices, angle, restitution);
         this.angle = angle;
+    }
+
+    @Override
+    public void switchState() {
+        rotate(90);
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        if (enabled) {
+            rotate(90);
+        }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void rotate(float angle) {
@@ -22,4 +39,5 @@ public abstract class RotatableChainObject extends ChainBodyObject {
     public void setAngle(float angle) {
         this.angle = angle;
     }
+
 }
